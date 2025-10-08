46d9d4f43ef199f69fc8729bc5075ec23c9a25f1
dgopinath3693 <dgopinath3693@gmail.com>
2025-10-08 17:20:45 -0500

    cleaned datasets and merged them

--- AICrawlerLogging/tracking.py
+++ AICrawlerLogging/tracking.py
@@ -2,3 +2,17 @@ import Pandas
 import NumPY
 import MatPlotlib
 
+server_df = pd.read_csv("server_logs.csv")
+bots_df = pd.read_csv("bots_table.csv")
+
+server_df['Date'] = pd.to_datetime(server_df['Date'])
+bots_df['Date'] = pd.to_datetime(bots_df['Date'])
+bots_df['Last hit date'] = pd.to_datetime(bots_df['Last hit date'])
+
+# server_df["Page path"]
+# bots_df["Page path"]
+
+merged = pd.merge(server_df, bots_df, on=["Date", "Page path"], how="left")
+merged["Page path"].head(30)
+merged = merged[merged["Bot"].notna() & (merged["Bot"] != "Unknown") & (merged["Page path"] != "/") & (merged["Page path"] != "/blank")]
+merged.dropna()
